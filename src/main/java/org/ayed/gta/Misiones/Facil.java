82647cd3a30f471488b02b4fd9f212aacb1dbeee
IvanaAbril <icano@fi.uba.ar>
2025-11-27 19:42:42 -0300

    Implementacion de los tipos de mision

--- src/main/java/org/ayed/gta/Misiones/Facil.java
+++ src/main/java/org/ayed/gta/Misiones/Facil.java
@@ -1,26 +1,44 @@
 package org.ayed.gta.Misiones;
 
 import org.ayed.gta.Garaje;
+import org.ayed.gta.Vehiculos.Vehiculo;
+import org.ayed.tda.vector.Vector;
 
 public class Facil extends Mision {
-	// private int recompensaDinero;
-	// private int recompensaCreditos;
+
+    private final int RECOMPENSA_DINERO = 1000;
+    private final int RECOMPENSA_CREDITOS = 10;
+    private static final double TIEMPO_MAX = 1000;
 
     public Facil() {
-        super(0);
+        super(TIEMPO_MAX);
     }
-	@Override 
-	public int vehiculosPermitidos(Garaje g){
-		return 0;
-	}
-	@Override
-	public int recompensaDinero() {
-		// IMPLEMENTAR
-		return 1;
-	}
-	@Override
-	public int recompensaCredito() {
-		//implementar
-		return 1;
-	}
+
+    @Override
+    public int vehiculosPermitidos(Garaje g) {
+
+        this.permitidos = new Vector<>();
+
+        Vector<Vehiculo> lista = g.obtenerVehiculo();
+
+        for (int i = 0; i < lista.tamanio(); i++) {
+            //todos los vehículos están permitidos
+            this.permitidos.agregar(lista.dato(i));
+        }
+
+        if (permitidos.tamanio() == 0)
+            return 0;
+
+        return permitidos.tamanio();
+    }
+
+    @Override
+    public int recompensaDinero() {
+        return RECOMPENSA_DINERO;
+    }
+
+    @Override
+    public int recompensaCredito() {
+        return RECOMPENSA_CREDITOS;
+    }
 }
