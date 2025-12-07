164be7235b15eb5f141d341de3ace2cf672d39e4
MVIVanessa <vimariani@fi.uba.ar>
2025-12-07 04:09:12 -0300

    Se añadió el gps a la interfaz

--- src/main/java/org/ayed/gta/Mapa/Interfaz.java
+++ src/main/java/org/ayed/gta/Mapa/Interfaz.java
@@ -13,13 +13,25 @@ import javafx.stage.Stage;
 public class Interfaz extends Application {
 
     private static final int TAM_CELDA = 35;
+    private Mapa mapa;
+    private Gps gps;
+    private GridPane gridPane;
 
     @Override
     public void start(Stage stage) {
-        Mapa mapa = new Mapa();
-        Lista<Lista<TipoCelda>> grilla = mapa.obtenerMapa();
+        mapa = new Mapa();
+        gps = new Gps(mapa);
 
-        GridPane gridPane = new GridPane();
+        gridPane = new GridPane();
+        dibujarMapa();
+
+        Scene scene = new Scene(gridPane);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void dibujarMapa() {
+        Lista<Lista<TipoCelda>> grilla = mapa.obtenerMapa();
         Iterador<Lista<TipoCelda>> recorridoFilas = grilla.iterador();
 
         int filaIndice = 0;
@@ -29,9 +41,10 @@ public class Interfaz extends Application {
             int columnaIndice = 0;
             while (recorridoColumnas.haySiguiente()) {
                 TipoCelda tipo = recorridoColumnas.dato();
+                Coordenadas coordenada = new Coordenadas(filaIndice, columnaIndice);
 
                 Rectangle rect = new Rectangle(TAM_CELDA, TAM_CELDA);
-                rect.setFill(colorPara(tipo));
+                rect.setFill(colorPara(tipo, coordenada));
                 rect.setStroke(Color.LIGHTGRAY);
 
                 gridPane.add(rect, columnaIndice, filaIndice);
@@ -41,14 +54,14 @@ public class Interfaz extends Application {
             recorridoFilas.siguiente();
             filaIndice++;
         }
-
-        Scene scene = new Scene(gridPane);
-        stage.setTitle("Mapa GTA - JavaFX");
-        stage.setScene(scene);
-        stage.show();
     }
 
-    private Color colorPara(TipoCelda tipo) {
+
+    private Color colorPara(TipoCelda tipo, Coordenadas coordenada) {
+        if ((tipo != TipoCelda.RECOMPENSA && tipo != TipoCelda.ENTRADA && tipo != TipoCelda.SALIDA)
+                && gps.buscarCoordenadas(coordenada)) {
+            return Color.YELLOW;}
+    
         switch (tipo) {
             case TRANSITABLE:   return Color.WHITE;
             case EDIFICIO:      return Color.BLACK;
@@ -56,9 +69,16 @@ public class Interfaz extends Application {
             case ENTRADA:       return Color.BLUE;
             case SALIDA:        return Color.GREEN;
             case RECOMPENSA:    return Color.MAGENTA;
-            //case GPS:           return Color.YELLOW;
             default:            return Color.GRAY;
         }
+    }
+    
+
+    // Llamar cada vez que el jugador se mueve
+    public void actualizarPosicionJugador(Coordenadas nuevaPosicion) {
+        gps.modificarPartida(nuevaPosicion);
+        gridPane.getChildren().clear(); // Limpia
+        dibujarMapa(); // Redibuja el mapa
     }
     public static void main(String[] args) {
         launch();
